6df05bd8added1d85d5e5651ef7b7eadc73f807b
Sunny22G <96507805+Sunny22G@users.noreply.github.com>
2021-12-25 19:46:33 +0530

    SD

---
+++ GREAT INDIA/PAGE/INDIA.js
@@ -0,0 +1,25 @@
+function view(icon)
+{
+var full = document.getElementById("full");
+full.src = icon.src;
+}
+function viewR(icon)
+{
+var full = document.getElementById("fullR");
+full.src = icon.src;
+}
+function view1(icon)
+{
+var full = document.getElementById("full1");
+full.src = icon.src;
+}
+function view2(icon)
+{
+var full = document.getElementById("full2");
+full.src = icon.src;
+}
+function view3(icon)
+{
+var full = document.getElementById("full3");
+full.src = icon.src;
+}
